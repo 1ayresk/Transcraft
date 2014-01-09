eff8eccdf3cca90baf07e0974d87c97590827298
modmuss50 <m123world@gmail.com>
2014-01-09 16:46:42 +0000

    cleaned up code

--- src/main/java/mark123mark/mods/transcraft/TileEntitys/Transcrafter/RenderHandTC.java
+++ src/main/java/mark123mark/mods/transcraft/TileEntitys/Transcrafter/RenderHandTC.java
@@ -49,8 +49,6 @@ public class RenderHandTC implements IItemRenderer
 	@Override
 	public void renderItem(ItemRenderType type, ItemStack item, Object... data) 
 	{
-		int renderType = 0;
-
 			if(type == type.ENTITY)
 			{
 				GL11.glPushMatrix();
@@ -66,7 +64,6 @@ public class RenderHandTC implements IItemRenderer
 			    model.Floaty_part.render(0.0625F);
 			    GL11.glPopMatrix();
 			}
-			
 			if(type == type.INVENTORY)
 			{
 				GL11.glDisable(GL11.GL_CULL_FACE);
@@ -75,8 +72,7 @@ public class RenderHandTC implements IItemRenderer
 	            GL11.glRotatef(50F, -1110.0F,0.0F, -1110.0F);
 	            GL11.glTranslatef((float) 1.0F, (float)-3.7f, (float) 1.4F);
 	            GL11.glScalef(13F, 13F, 13F);
-	            Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("transcraft", "textures/models/TranscrafterModel.png"));
-			            
+	            Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("transcraft", "textures/models/TranscrafterModel.png"));     
 			    model.Middle.render(0.0625F);
 			    model.Top_bit_1.render(0.0625F);
 			    model.Top_bit_2.render(0.0625F);
@@ -86,64 +82,42 @@ public class RenderHandTC implements IItemRenderer
 			    GL11.glDisable(GL11.GL_BLEND);
 			    GL11.glPopMatrix();
 			}
-			
 			if(type == type.EQUIPPED_FIRST_PERSON)
 			{
 				GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-				
 				GL11.glPushMatrix();
-				
 	            GL11.glEnable(GL11.GL_BLEND);
 	            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-			
 	            GL11.glRotatef(180F, 0.0F, 0.0F, 1.0F);
-	            
 	            GL11.glTranslatef((float) - 0.4F, (float)- 1.5f, (float) 0.4F);
-	            
 	            GL11.glScalef(1F, 1F, 1F);
-	          
-	            Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("transcraft", "textures/models/TranscrafterModel.png"));
-			            
+	            Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("transcraft", "textures/models/TranscrafterModel.png"));       
 			    model.Middle.render(0.0625F);
 			    model.Top_bit_1.render(0.0625F);
 			    model.Top_bit_2.render(0.0625F);
 			    model.Top_bit_3.render(0.0625F);
 			    model.Top_bit_4.render(0.0625F);
 			    model.Floaty_part.render(0.0625F);
-			    
 			    GL11.glPopMatrix();
 			}
-			 
 			if(type == type.EQUIPPED)
 			{
 				GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-				
 				GL11.glPushMatrix();
-				
 	            GL11.glEnable(GL11.GL_BLEND);
 	            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-			
 	            GL11.glRotatef(180F, 0.0F, 0.0F, 1.0F);
-	            
 	            GL11.glTranslatef((float) - 0.4F, (float)- 1.5f, (float) 0.4F);
-	            
 	            GL11.glScalef(1F, 1F, 1F);
-	          
-	            Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("transcraft", "textures/models/TranscrafterModel.png"));
-			            
+	            Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("transcraft", "textures/models/TranscrafterModel.png"));     
 			    model.Middle.render(0.0625F);
 			    model.Top_bit_1.render(0.0625F);
 			    model.Top_bit_2.render(0.0625F);
 			    model.Top_bit_3.render(0.0625F);
 			    model.Top_bit_4.render(0.0625F);
 			    model.Floaty_part.render(0.0625F);
-			    
-		        
 	            GL11.glDisable(GL11.GL_BLEND);
-				
 				GL11.glPopMatrix();
-
-			}
-				
+			}	
 		}
 }
