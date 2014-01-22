3d8f955947464d5abf4e6006dd5442323a708a9d
modmuss50 <m123world@gmail.com>
2014-01-22 17:22:16 +0000

    Forgot to save this!

--- src/main/java/modmuss50/mods/transcraft/Blocks/TranscraftOreItem.java
+++ src/main/java/modmuss50/mods/transcraft/Blocks/TranscraftOreItem.java
@@ -2,50 +2,49 @@ package modmuss50.mods.transcraft.Blocks;
 
 import java.util.List;
 
-import org.lwjgl.input.Keyboard;
-
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import modmuss50.mods.transcraft.helpers.Config;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 
+import org.lwjgl.input.Keyboard;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
 public class TranscraftOreItem extends ItemBlock {
 
-	
-	public TranscraftOreItem(int id)
-	{
+	public TranscraftOreItem(int id) {
 		super(id);
 		setHasSubtypes(true);
 	}
-	
+
 	@Override
-	public int getMetadata (int damageValue) {
+	public int getMetadata(int damageValue) {
 		return damageValue;
 	}
 
 	@Override
 	public String getUnlocalizedName(ItemStack itemstack) {
-		return getUnlocalizedName() + "." + TranscraftOre.types[itemstack.getItemDamage()].replace(" ", "").toLowerCase();
+		return getUnlocalizedName()
+				+ "."
+				+ TranscraftOre.types[itemstack.getItemDamage()].replace(" ",
+						"").toLowerCase();
 	}
 
-	 @Override
-		@SideOnly(Side.CLIENT)
-		public void addInformation(ItemStack stack, EntityPlayer player, List list,boolean par4) {
-			if(Config.enderswordpearl == true)
-			{
-				if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT) || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT))
-		        {
-					list.add("\u00a72This ore is part of transcraft");
-					list.add("\u00a72You can randomly find it in the ground");
-		        }
-				else
-				{
-					list.add("\u00a72[\u00a74SHIFT\u00a72]");
-				}
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, EntityPlayer player, List list,
+			boolean par4) {
+		if (Config.enderswordpearl == true) {
+			if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)
+					|| Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
+				list.add("\u00a72This ore is part of transcraft");
+				list.add("\u00a72You can randomly find it in the ground");
+			} else {
+				list.add("\u00a72[\u00a74SHIFT\u00a72]");
 			}
 		}
-	
-	
+	}
+
 }
